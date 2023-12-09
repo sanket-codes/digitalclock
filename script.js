3d96042b3291ab79068ed09165afa752db5e8f25
Sanket Mhatre <111676569+sanket-codes@users.noreply.github.com>
2023-12-09 06:23:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const hour = document.querySelector("#hour");
+const min = document.querySelector("#min");
+const sec = document.querySelector("#sec");
+const ap = document.querySelector("#ap");
+
+const dot = document.querySelector("#dot");
+
+console.dir(new Date());
+
+setInterval(() => {
+  const currentTime = new Date();
+
+  let currentHour = currentTime.getHours();
+
+  if (currentHour == 0) {
+    currentHour = 12;
+    ap.innerHTML = "AM";
+  } else if (currentHour > 12) {
+    currentHour -= 12;
+    ap.innerHTML = "PM";
+  } else {
+    ap.innerHTML = "AM";
+  }
+
+  hour.innerHTML = (currentHour < 10 ? "0" : "") + currentHour;
+  min.innerHTML =
+    (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+  sec.innerHTML =
+    (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
+}, 1000);
